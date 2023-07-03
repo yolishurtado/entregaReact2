efd7ee687345dfd5019e76c4205ef8a16de6841f
speedbuild98 <lautaropectm98@gmail.com>
2023-07-03 15:39:45 -0300

    Fixed imports

--- src/components/ItemDetailContainer/ItemDetailContainer.jsx
+++ src/components/ItemDetailContainer/ItemDetailContainer.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react'
-import { getUnProducto } from '../../asyncmock'
+import { getUnProducto } from '../../asynmock'
 import ItemDetail from '../ItemDetail/ItemDetail';
 import { useParams } from 'react-router-dom';
 
